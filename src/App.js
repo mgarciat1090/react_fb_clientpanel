78b27c4cb02edbd17852650fc3d490173d4b8d10
mgarciat1090 <mgarciat1090@gmail.com>
2019-10-08 01:41:07 -0500

    Adding client detail

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import store from './store';
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
+import ClientDetails from './components/clients/ClientDetail';
+
 import './App.css';
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
             <Switch>
               <Route exact path="/" component={Dashboard} />
               <Route exact path="/client/add" component={AddClient} />
+              <Route exact path="/client/:id" component={ClientDetails} />
             </Switch>
           </div>
         </div>
